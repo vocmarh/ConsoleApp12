18a6398f628f17434a983b7125674c7fee16e39d
Сергей Храмцов <vocmarh@mail.ru>
2022-04-20 18:13:42 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetMarket
+{
+    abstract class Delivery
+    {
+        public string Address;
+    }
+    class HomeDelivery : Delivery
+    {
+        
+    }
+    class PickPointDelivery : Delivery
+    {
+        
+    }
+    class ShopDelivery : Delivery
+    {
+        
+    }
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
